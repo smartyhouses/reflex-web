ab6029719761ca486c74a39f682cd7691717d854
Your Name <you@example.com>
2024-10-24 14:12:06 +0300

    cmt

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,9 @@ import reflex as rx
 from pcweb.styles.tailwind_config import tw_config
 
 config = rx.Config(
-    port=8080,
+    port=3000,
     app_name="pcweb",
-    deploy_url="http://0.0.0.0:8080",
+    deploy_url="http://0.0.0.0:3000",
     frontend_packages=[
         "chakra-react-select",
         "@radix-ui/react-navigation-menu",
