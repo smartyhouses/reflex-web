df00f666f4df5bb67d709fd67c1bc452830e67e3
Your Name <you@example.com>
2024-10-24 13:23:40 +0300

    cmt

--- pcweb/components/webpage/badge.py
+++ pcweb/components/webpage/badge.py
@@ -10,12 +10,12 @@ def badge() -> rx.Component:
         content=rx.link(
             get_icon("badge_logo"),
             rx.text(
-                "Built with Reflex",
+                "АО Портал",
                 class_name="text-slate-1 font-semibold font-['Instrument_Sans'] text-sm leading-4 tracking-[-0.00656rem]",
             ),
             underline="none",
             class_name="fixed bottom-4 right-4 flex flex-row gap-1.5 items-center w-auto rounded-lg dark:bg-[#FCFCFD] shadow-small p-1.5 transition-bg border border-solid dark:border-[#E0E1E6] z-[9998] bg-[#151618] border-[#27282B] cursor-pointer",
-            href="https://github.com/reflex-dev/reflex-web",
+            href="https://prtl.cc",
         ),
         align="start",
     )
